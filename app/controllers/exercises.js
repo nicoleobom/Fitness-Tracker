99f2a15e5a3340df522093286c25c8b764f8e373
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-06-27 10:47:14 -0400

    working on exercise controller

--- app/controllers/exercises.js
+++ app/controllers/exercises.js
@@ -11,4 +11,34 @@ router.post('/submit', ({body}, res) => {
     }).catch(error => {
         res.json(error);
     })
-}
+});
+
+router.get('/exercises', (req, res) => {
+    db.Exercises.find({}).sort({_id: 'description'})
+    .then(dbExercises => {
+        res.json(dbExercises);
+    }).catch(error => {
+        res.json(error);
+    })
+})
+
+// Find by ID
+router.get('/exercises/:id', (req, res) => {
+    db.Exercises.findById(req.params.id).then(result => {
+        if(!result) {
+            return res.status(404).send({
+                message: 'Exercise not found. Invalid ID.'
+            });
+        }
+        res.send(result);
+    }).catch(error => {
+        if (error.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: 'Exercise not found. Invalid ID.'
+            })
+        }
+        return res.status(500).send({
+            message: 'Error finding exercise with that ID.'
+        })
+    })
+});
