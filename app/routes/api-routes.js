d1ec30841d4c2834c0faf2b7d9ce529193ea4a31
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-06-28 14:33:27 -0400

    trying async functions

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,12 +1,26 @@
 var db = require('../models');
 
 module.exports = function(app) {
+
+    // get route for last workout thru api.js file
     app.get('/api/workouts', function(req, res) {
         db.Workouts.find({}).then(workout => {
             res.json(workout);
         }).catch(error => {
             res.json(error);
         })
+    });
+
+    //create new workout, post to api via json
+    app.post('/api/workouts', await function(req, res) {
+        try{
+            const response = await db.Workouts.create({
+                type: 'workout',
+            });
+            res.json(response);
+        } catch(error) {
+            console.log('Error occured when trying to create this workout. Please try again.')
+        }
     })
 }
 
